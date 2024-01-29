78f40918455d2e0b3e602fe4132cf9f86e977641
Sanjana <sanjana.thapa@netsmartz.net>
2024-01-29 17:04:47 +0530

    react-date-picker-range

---
+++ src/components/DateRangePickerComp.js
@@ -0,0 +1,32 @@
+import React, { useReducer } from "react";
+import { DateRangePicker } from "react-date-range";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+
+const DateRangePickerComp = () => {
+  const [localState, setLocalState] = useReducer((prevState, newState) => ({ ...prevState, ...newState }), {
+    startDate: new Date(),
+    endDate: new Date(),
+    key: "selection",
+  });
+
+  const { startDate, endDate, key } = localState;
+
+  const handleSelect = (range) => {
+    console.log(range, "ranges---------------");
+    setLocalState({
+      startDate: range?.selection?.startDate,
+      endDate: range?.selection?.endDate,
+    });
+  };
+
+  const selectionRange = {
+    startDate,
+    endDate,
+    key,
+  };
+
+  return <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />;
+};
+
+export default DateRangePickerComp;
